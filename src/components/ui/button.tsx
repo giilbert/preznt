a390b9f4e9777dc68364606ff5141585255b1045
Gilbert Zhang <gilbertzhang1208@gmail.com>
2022-09-27 19:35:21 -0400

    fix loading spinner badness

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -90,7 +90,7 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
         <button
           ref={ref}
           className={clsx(
-            `font-medium transition-all truncate`,
+            `font-medium transition-all truncate flex justify-center`,
             sizes[size],
             variants[variant],
             !disabled && !loading && `${variantHover[variant]} active:scale-95`,
--- src/components/util/spinner.tsx
+++ src/components/util/spinner.tsx
@@ -1,6 +1,6 @@
 export const Spinner: React.FC = () => (
   <svg
-    className="text-white animate-spin w-5 h-5"
+    className="text-white animate-spin w-6 h-6"
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
