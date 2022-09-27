ff674e40a1f1503cf42fa77a544b6af416eb167c
Gilbert Zhang <gilbertzhang1208@gmail.com>
2022-09-27 10:34:37 -0400

    attributes display

--- src/components/organizations/attributes-list.tsx
+++ src/components/organizations/attributes-list.tsx
@@ -1,10 +1,17 @@
 import { trpc } from "@/utils/trpc";
-import { Text } from "@/components/ui";
+import { Text, Heading } from "@/components/ui";
 import { useOrganization } from "@/lib/use-organization";
+import clsx from "clsx";
+
+const tableCellClasses = "border-2 border-neutral-800 px-6 py-2";
 
 export const AttributesList: React.FC = () => {
   const organization = useOrganization();
-  const { data, status, error } = trpc.organization.getSelfAttributes.useQuery({
+  const {
+    data: attributes,
+    status,
+    error,
+  } = trpc.organization.getSelfAttributes.useQuery({
     organizationId: organization.id,
   });
 
@@ -13,8 +20,36 @@ export const AttributesList: React.FC = () => {
 
   return (
     <div>
-      <Text>Attributes displayed in the form of JSON.stringify: </Text>
-      <Text>{JSON.stringify(data)}</Text>
+      <Heading>Attributes</Heading>
+      {attributes.length > 0 ? (
+        <table className="bg-background-secondary border-spacing-x-5 mt-2">
+          <thead>
+            <tr>
+              <th
+                className={clsx(tableCellClasses, "font-bold w-48 text-start")}
+              >
+                ATTRIBUTE
+              </th>
+              <th className={clsx(tableCellClasses, "font-bold text-start")}>
+                VALUE
+              </th>
+            </tr>
+          </thead>
+
+          {attributes.map((attribute) => (
+            <tr key={attribute.name}>
+              <td className={clsx(tableCellClasses, "font-mono")}>
+                {attribute.name}
+              </td>
+              <td className={tableCellClasses}>{attribute.value}</td>
+            </tr>
+          ))}
+        </table>
+      ) : (
+        <p className="text-gray-300">
+          You don&apos;t have any attributes, get some by redeeming Preznts!
+        </p>
+      )}
     </div>
   );
 };
