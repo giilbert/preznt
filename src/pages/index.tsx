81219a57d441466c1bad6cdac6e5e1eaa3e23a1c
Gilbert Zhang <gilbertzhang1208@gmail.com>
2022-10-15 15:05:32 -0400

    quick and dirty landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,27 @@ import { CreateOrganization } from "@/components/organizations/create";
 import { Button } from "@/components/ui";
 import { JoinOrganization } from "@/components/organizations/join";
 import { Layout } from "@/components/layout/layout";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   const { status } = useSession();
 
   if (status === "loading") return <p>Defrosting lambda functions...</p>;
   if (status === "unauthenticated")
-    return <Button onClick={() => signIn("google")}>Sign in</Button>;
+    return (
+      <div className="flex justify-center items-center h-screen flex-col gap-8">
+        <Image
+          src="/large-logo.svg"
+          width={1059.2 / 3}
+          height={282.84 / 3}
+          alt="Large preznt logo"
+        />
+
+        <Button onClick={() => signIn("google")} className="w-24">
+          Sign in
+        </Button>
+      </div>
+    );
 
   return (
     <Layout
