d5883fd4a5a8fa0c9060b4d08e420726f930ca91
Gilbert Zhang <gilbertzhang1208@gmail.com>
2022-10-16 10:12:01 -0400

    add link to member page from redeemer

--- src/components/preznt/list-redeemers.tsx
+++ src/components/preznt/list-redeemers.tsx
@@ -2,6 +2,8 @@ import { Card, Heading } from "../ui";
 import Image from "next/image";
 import { PrezntOnUser } from "@prisma/client";
 import moment from "moment";
+import { useOrganization } from "@/lib/use-organization";
+import Link from "next/link";
 
 type Redeemer = PrezntOnUser & {
   user: {
@@ -14,25 +16,37 @@ type Redeemer = PrezntOnUser & {
 export const ListPrezntRedeemers: React.FC<{
   redeemers: Redeemer[];
 }> = ({ redeemers }) => {
+  const organization = useOrganization();
+
   return (
     <div>
       {redeemers.map(({ user, ...redeemer }) => (
-        <Card
-          key={user.email}
-          className="bg-neutral-800 flex gap-4 items-center"
+        <Link
+          href={{
+            pathname: "/[slug]/member/[id]",
+            query: {
+              slug: organization.slug,
+              id: redeemer.userId,
+            },
+          }}
+          key={redeemer.userId}
         >
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            src={user.image || ""}
-            className="rounded-full w-10 h-10"
-            alt=""
-          />
-          <p className="font-bold">{user.name}</p>
+          <a>
+            <Card className="bg-neutral-800 flex gap-4 items-center">
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                src={user.image || ""}
+                className="rounded-full w-10 h-10"
+                alt=""
+              />
+              <p className="font-bold">{user.name}</p>
 
-          <p className="text-neutral-300">
-            {moment(redeemer.redeemedAt).calendar()}
-          </p>
-        </Card>
+              <p className="text-neutral-300">
+                {moment(redeemer.redeemedAt).calendar()}
+              </p>
+            </Card>
+          </a>
+        </Link>
       ))}
     </div>
   );
